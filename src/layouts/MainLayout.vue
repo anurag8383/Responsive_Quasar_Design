078d5943f51b56d1b315dafdc549ab0a3725713f
anurag8383 <83387984+anurag8383@users.noreply.github.com>
2022-08-26 15:59:11 +0530

    Responsiveness

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,90 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-resize-observer @resize="onResize"/>
+    <q-header>
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-img class="imge" src="https://t3.ftcdn.net/jpg/04/43/10/12/360_F_443101282_x88vQ5SdTGNKxx36RelWJ7crqRk5trSE.jpg"
+            style="width: 120px; border-radius: 30%; height: 70px; " />
+        </q-toolbar-title>
+
+        <div class="gt-sm">
+          <q-btn outline rounded label="Home" class="q-mr-sm" />
+          <q-btn outline rounded label="SignIn" class="q-mr-sm" />
+          <q-btn outline rounded label="SignUp" class="q-mr-sm" />
+
+        </div>
+        <div class="lt-md">
+          <q-btn flat dense round icon="menu" aria-label="Menu" @click="toggleLeftDrawer" />
+        </div>
+      </q-toolbar>
+    </q-header>
+    <q-drawer v-model="leftDrawerOpen" bordered content-class="bg-grey-2">
+      <q-list>
+        <q-item-label header>
+          Essential Links
+        </q-item-label>
+        <EssentialLink v-for="link in essentialLinks" :key="link.title" v-bind="link" />
+        <q-item clickable tag="a" target="_blank"
+          href="https://www.youtube.com/watch?v=fN2-1V2YARQ&list=PLAl8os33KyOXvyPhuO1gHr6YVlTZXzKiS&index=3">
+          <q-item-section avatar>
+            <q-icon name="Home"></q-icon>
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Home</q-item-label>
+            <q-item-label caption>quasar.dev</q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </q-drawer>
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+import EssentialLink from 'components/EssentialLink.vue'
+
+
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  components: {
+    EssentialLink,
+  },
+
+  setup() {
+    const leftDrawerOpen = ref(false)
+
+    // return {
+    //   essentialLinks: linksList,
+    //   leftDrawerOpen,
+    //   toggleLeftDrawer() {
+    //     leftDrawerOpen.value = !leftDrawerOpen.value
+    //   }
+    // }
+    return {
+      leftDrawerOpen,
+      toggleLeftDrawer() {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      }
+    }
+  }
+})
+</script>
+<style scoped>
+.imge{
+  opacity: 0.6;
+  border: none;
+  height: 100vh;
+  object-fit: cover;
+  margin-top: 1%;
+  margin-bottom: 1%;
+}
+/* .btn {
+  font-family: "Trirong", serif;
+} */
+</style>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,61 @@
+<template>
+  <q-page class="bg">
+    <div class="row bg-primary">
+      <div class="col">
+        <div class="prac ">
+          <div
+            class="text-h4 text-bold text-black q-mt-xl q-pb-lg q-pl-lg"
+            style="margin-left:auto; margin-right: auto; display: block;"
+          >
+            Learn How to make this beautiful ui by AS (ANURAG SINGH)
+          </div>
+          <div class="text-h5 text-bold text-blue q-pb-sm q-pl-lg q-pr-lg">
+            Join the AS academy today for free for these amazing contents
+          </div>
+          <q-input
+            color="lime-11"
+            bg-color="green"
+            filled
+            placeholder="Your Email"
+            class="q-pl-lg q-pr-lg q-pb-lg"
+            style="font-family:'Courier New', Courier, monospace;"
+          >
+            <template v-slot:prepend>
+              <q-icon name="email" />
+            </template>
+            <template v-slot:append>
+              <q-btn label="Start Learning" color="purple"></q-btn>
+            </template>
+          </q-input>
+        </div>
+      </div>
+      <div class="col gt-sm">
+        <q-img src="https://images.cdn4.stockunlimited.net/preview1300/academy-logo-design_1968150.jpg" style="height: 28%; width: 30%; margin-top:8%; margin-left: auto; margin-right:auto; display: block;"
+        />
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "IndexPage",
+});
+</script>
+<style scoped>
+
+/* .emge {
+  background-image: url(https://images.pexels.com/photos/301926/pexels-photo-301926.jpeg?cs=srgb&dl=pexels-pixabay-301926.jpg&fm=jpg);
+} */
+
+.prac{
+  text-align: center;
+  max-width: 650px;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+
+</style>
